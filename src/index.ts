6abedd4f4a4cff1eb3663a912474f5475a08357f
Alexander Weidt <alexw94ks@yahoo.de>
2020-06-10 20:26:48 +0200

    add highly experimental SuspenseStore

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from './entity/EntityStore';
 export * from './entity/SelectEntityStore';
 export * from './entity/DynamicLoadingStore';
 export * from './entity/PersistentEntityStore';
+export * from './entity/SuspenseStore';
 export * from './entity/util';
